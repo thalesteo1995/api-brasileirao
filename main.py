0d48ff22b92c223d09a0b33d46ebceb20d439813
Thales Teodoro Ampere <thales@ampereconsultoria.com.br>
2022-05-09 22:16:34 -0300

    feat: implementa s01_webscraping_cbf.py

--- main.py
+++ main.py
@@ -10,11 +10,20 @@
 from dynaconf import Dynaconf
 from setups.s01_webscraping_cbf import main as s01_webscraping_cbf
 
+settings = Dynaconf(
+    envvar_prefix="BRASILEIRO",
+    settings_files=["settings.toml"],
+    environments=True,
+    load_dotenv=True,
+)
+
 
 def main():
     """_summary_"""
+
     # s01
-    s01_webscraping_cbf()
+    if settings.S01:
+        s01_webscraping_cbf()
 
 
 if __name__ == "__main__":
--- setups/s01_webscraping_cbf.py
+++ setups/s01_webscraping_cbf.py
@@ -7,12 +7,75 @@
     Author:           @Thales
     Created:          2022-05-08
 """
+import sys
+import logging
+import requests
+import re
+import json
+
+from pathlib import Path
+from dynaconf import Dynaconf
+from tqdm import tqdm
+from bs4 import BeautifulSoup
+
+settings = Dynaconf(
+    envvar_prefix="BRASILEIRO",
+    settings_files=["settings.toml"],
+    environments=True,
+    load_dotenv=True,
+)
 
 
 def main():
     """_summary_"""
-    print("Realizando Webscraping...")
 
+    Path("logs").mkdir(exist_ok=True, parents=True)
+    logging.basicConfig(
+        filename=f"logs/{settings.LOG_MONITOR}", filemode="w", level=logging.DEBUG
+    )
+    lst_dicts = []
+    pbar_convert = tqdm(total=settings.ANO_FINAL - settings.ANO_INICIAL + 1)
+    for ano in range(settings.ANO_INICIAL, settings.ANO_FINAL + 1):
+        url_cbf = f"{settings.BASE_URL_CBF}/{ano}"
+        try:
+            soup = BeautifulSoup(requests.get(url_cbf).text, features="html.parser")
+        except requests.exceptions.ConnectionError as err:
+            logging.error(err)
+            logging.error("Erro de Conexão com  Internet!")
+            sys.exit()
 
-if __name__ == "__main__":
-    main()
+        for id_clube_tabela in range(0, 20):
+            clube = soup.find_all(class_="expand-trigger")[id_clube_tabela].get_text()
+            string_clube = re.sub(r"\n", "/", clube)
+            lst_info_clube = string_clube.split("/")
+            nome_clube = lst_info_clube[6]
+            num_pontos = lst_info_clube[8]
+            num_vitorias = lst_info_clube[10]
+            num_empates = lst_info_clube[11]
+            num_derrotas = lst_info_clube[12]
+            gols_pro = lst_info_clube[13]
+            gols_contra = lst_info_clube[14]
+
+            data = {
+                f"Brasileirao {ano}": {
+                    nome_clube: {
+                        "pontos": num_pontos,
+                        "vitorias": num_vitorias,
+                        "empates": num_empates,
+                        "derrotas": num_derrotas,
+                        "gols_pro": gols_pro,
+                        "gols_contra": gols_contra,
+                    }
+                }
+            }
+
+            lst_dicts.append(data)
+            logging.info(f"Brasileirao {ano} - Finalizado!")
+
+        pbar_convert.update(1)
+    pbar_convert.close()
+
+    with open(settings.ARQUIVO_JSON, "w", encoding="UTF-8") as f:
+        json.dump(lst_dicts, f)
+
+    logging.info(f"ARQUIVO {settings.ARQUIVO_JSON} CRIADO!!!")
